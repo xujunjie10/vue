14527ced663908dbda17e7401167b6136642ad56
xujunjie10 <xu610152296@126.com>
2018-11-09 00:07:53 +0800

    new project login

---
+++ src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="login">
+    <el-form class="container" :model="loginForm" :rules="myrules">
+      <el-form-item class="avatar">
+        <img src="../assets/avatar.jpg" alt="">
+      </el-form-item>
+      <el-form-item prop="username">
+        <el-input prefix-icon="myicon myicon-user" v-model="loginForm.username"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input prefix-icon="myicon myicon-key" v-model="loginForm.password" type="password"></el-input>
+      </el-form-item>
+      <div>
+          <el-button type="primary" class="login-btn">登录</el-button>
+      </div>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      myrules: {
+        username: { required: true, message: '请输入用户名', trigger: 'blur' },
+        password: { required: true, message: '请输入密码', trigger: 'blur' }
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .login {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: #2f4050;
+
+    .container {
+      position: absolute;
+      left: 0;
+      right: 0;
+      width: 400px;
+      padding: 0px 40px 15px 40px;
+      margin: 200px auto;
+      background: white;
+      .avatar {
+        position: relative;
+        left: 50%;
+        width: 120px;
+        height: 120px;
+        margin-left: -60px;
+        margin-top: -60px;
+        box-sizing: border-box;
+        border-radius: 50%;
+        border: 10px solid #fff;
+        box-shadow: 0 1px 5px #ccc;
+        overflow: hidden;
+      }
+      .login-btn {
+        width: 100%;
+      }
+    }
+  }
+</style>
